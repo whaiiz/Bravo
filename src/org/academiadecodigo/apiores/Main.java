c0d18405a9f87a25aef13bfd0d57538deb092dd2
jaime <jaime_reis18@outlook.pt>
2020-02-08 15:01:22 -0100

    Projeto base com simple library

--- src/org/academiadecodigo/apiores/Main.java
+++ src/org/academiadecodigo/apiores/Main.java
@@ -1,4 +1,11 @@
 package org.academiadecodigo.apiores;
 
+import org.academiadecodigo.simplegraphics.pictures.Picture;
+
 public class Main {
+
+    public static void main(String[] args) {
+       // Picture p1 = new Picture();
+    }
+
 }
