6d2dd467e25853908371c644d7d73b7a466bb4f4
HugoMarques <hugomelo12@hotmail.com>
2020-02-09 20:41:30 -0100

    draw and select picture improved

--- src/org/academiadecodigo/apiores/game/Play.java
+++ src/org/academiadecodigo/apiores/game/Play.java
@@ -51,13 +51,13 @@ public class Play {
 
             if (answer == Alu.getCorrectAnswer()) {
                 //correctSymbol.draw();
-                currentPlayer.move();
+                //currentPlayer.move();
 
                 //precisamos de um delay!!
 
 
            }
-            currentPlayer.move();
+            //currentPlayer.move();
         System.out.println("teste 4");
 
        // }
@@ -69,101 +69,90 @@ public class Play {
 
         switch (i) {
             case 0:
-                Picture firstNumber0 = new Picture(370, 50, "resources/0.png");
-                return firstNumber0;
+                return new Picture(370, 50, "resources/0.png");
             //firstNumber0.draw();
             //break;
             case 1:
-                Picture firstNumber1 = new Picture(375, 50, "resources/1.png");
-                return firstNumber1;
+                return new Picture(375, 50, "resources/1.png");
             //firstNumber1.draw();
             //break;
             case 2:
-                Picture firstNumber2 = new Picture(390, 50, "resources/2.png");
-                return firstNumber2;
+                return new Picture(390, 50, "resources/2.png");
             //firstNumber2.draw();
             //break;
             case 3:
-                Picture firstNumber3 = new Picture(380, 50, "resources/3.png");
-                return firstNumber3;
+                return new Picture(380, 50, "resources/3.png");
             //firstNumber3.draw();
             //break;
             case 4:
-                Picture firstNumber4 = new Picture(380, 50, "resources/4.png");
-                return firstNumber4;
+                return new Picture(380, 50, "resources/4.png");
             //firstNumber4.draw();
             //break;
             case 5:
-                Picture firstNumber5 = new Picture(380, 50, "resources/5.png");
-                return firstNumber5;
+                return new Picture(380, 50, "resources/5.png");
             //firstNumber5.draw();
             //break;
             case 6:
-                Picture firstNumber6 = new Picture(375, 50, "resources/6.png");
-                return firstNumber6;
+                return new Picture(375, 50, "resources/6.png");
             //firstNumber6.draw();
             //break;
             case 7:
-                Picture firstNumber7 = new Picture(380, 50, "resources/7.png");
-                return firstNumber7;
+                return new Picture(380, 50, "resources/7.png");
             //firstNumber7.draw();
             //break;
             case 8:
-                Picture firstNumber8 = new Picture(365, 50, "resources/8.png");
-                return firstNumber8;
+                return new Picture(365, 50, "resources/8.png");
             //firstNumber8.draw();
             //break;
             case 9:
-                Picture firstNumber9 = new Picture(370, 50, "resources/9.png");
-                return firstNumber9;
+                return new Picture(370, 50, "resources/9.png");
             //firstNumber9.draw();
             //break;
             case 10:
-                Picture firstNumber10 = new Picture(370, 50, "resources/10.png");
-                return firstNumber10;
+                return new Picture(370, 50, "resources/10.png");
             //firstNumber10.draw();
             //break;
         }
         return null;
     }
 
-    private void drawFirstPicture(int counts[]) {
+    private void drawFirstPicture(int arrayNumber0) {
 
-        if(counts[0] == 10) {
+        if(arrayNumber0 == 10) {
 
-            Picture ten = selectPicture(counts[0]);
+            Picture ten = selectPicture(arrayNumber0);
             ten.translate(-40,0);
             ten.draw();
         }else {
-            selectPicture(counts[0]).draw();
+            selectPicture(arrayNumber0).draw();
         }
+
+
+
+    }
+
+    private void drawSecondPicture(int arrayNumber1){
+        Picture picture = selectPicture(arrayNumber1);
+        picture.translate(150, 0);
+        picture.draw();
+    }
+
+    public void drawNumbers(int counts[]) {
+
+        drawFirstPicture(counts[0]);
+
         Picture multiplication = new Picture(450, 50, "resources/x.png");
         multiplication.draw();
 
-    }
+        drawSecondPicture(counts[1]);
 
-    private void drawSecondPicture(int counts[]){
-        Picture picture = selectPicture(counts[1]);
-        picture.translate(150, 0);
-        picture.draw();
-        Picture iqual = new Picture(600, 50, "resources/=.png");
-        if(counts[1] == 10){
-            iqual.translate(20,0);
-            iqual.draw();
+        Picture equal = new Picture(600, 50, "resources/=.png");
+
+        if (counts[1] == 10) {
+            equal.translate(20, 0);
         }
 
-        iqual.draw();
-    }
-
-    public void drawNumbers(int counts[]){
-        
-        drawFirstPicture(counts);
-        drawSecondPicture(counts);
-    }
-
-
-
-    public void show(){
+        equal.draw();
 
     }
 
